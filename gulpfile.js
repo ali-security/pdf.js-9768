8ba9e4cf4f947c794b880c7d1e2e710ac6731ebd
galgolamiel <gal.g@sealsecurity.io>
2025-06-01 09:38:40 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -2029,7 +2029,7 @@ gulp.task(
 );
 
 function packageBowerJson() {
-  const VERSION = getVersionJSON().version;
+  const VERSION = getVersionJSON().version+"-sp1";
 
   const DIST_NAME = "pdfjs-dist";
   const DIST_DESCRIPTION = "Generic build of Mozilla's PDF.js library.";
@@ -2209,7 +2209,7 @@ gulp.task(
       opts.cwd = installPath;
       distPath = path.relative(installPath, distPath);
     }
-    safeSpawnSync("npm", ["install", distPath], opts);
+    safeSpawnSync("npm", ["install", "--force", distPath], opts);
     done();
   })
 );
@@ -2350,8 +2350,7 @@ gulp.task("externaltest", function (done) {
 gulp.task(
   "ci-test",
   gulp.series(
-    gulp.parallel("lint", "externaltest", "unittestcli"),
-    "lint-chromium",
+    gulp.parallel("externaltest", "unittestcli"),
     "typestest"
   )
 );
